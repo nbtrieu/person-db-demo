de55eefd0d43ba87af3aff86099341fbc2dc1eb4
Nicole T <nbtrieu@uci.edu>
2024-02-28 15:10:23 -0800

    testing add nodes and edges functions

--- app/gremlin_queries.py
+++ app/gremlin_queries.py
@@ -51,10 +51,10 @@ def add_people(g: GraphTraversalSource, contact_df: pd.DataFrame):
         total=contact_df.shape[0],
         desc="Importing People"
     ):
-        uid_value = row["Email"] if pd.notnull(row["Email"]) else "_".join([row["Full Name"], row["Organization"]]).replace(" ", "_").lower()
-        
+        person_uid_value = row["Email"] if pd.notnull(row["Email"]) else "_".join([row["Full Name"], row["Organization"]]).replace(" ", "_").lower()
+
         person_properties = {
-            Person.PropertyKey.UID: uid_value,
+            Person.PropertyKey.UID: person_uid_value,
             Person.PropertyKey.NAME: row.get("Full Name", None),
             Person.PropertyKey.EMAIL: row.get("Email", None),
             Person.PropertyKey.PHONE: row.get("Phone", None),
@@ -101,10 +101,12 @@ def add_organization(g: GraphTraversalSource, contact_df: pd.DataFrame):
         else:
             domain_value = None
             acronym_value = None
+        
+        organization_name_value = row["Organization"].lower().capitalize()
 
         organization_properties = {
-            Organization.PropertyKey.UID: acronym_value,
-            Organization.PropertyKey.NAME: row["Organization"],
+            Organization.PropertyKey.UID: organization_name_value,
+            Organization.PropertyKey.NAME: organization_name_value,
             Organization.PropertyKey.DOMAIN: domain_value,
             Organization.PropertyKey.ACRONYM: acronym_value,
             ** ({Organization.PropertyKey.MAILING_ADDRESS: row["Organization's Mailing Address"]} if "Organization's Mailing Address" in contact_df.columns and not pd.isnull(row.get("Organization's Mailing Address")) else {})
@@ -114,4 +116,47 @@ def add_organization(g: GraphTraversalSource, contact_df: pd.DataFrame):
             label=Organization.LABEL,
             properties=organization_properties
         )
-    
+
+    query_executor.force_execute()
+
+    organization_node_list = (
+        g.V()
+        .has_label("organization")
+        .project("id", "uid")
+        .by(__.id_())
+        .by(__.values("uid"))
+        .to_list()
+    )
+    organization_id_dict = {
+        organization_node["uid"]: organization_node["id"] for organization_node in organization_node_list
+    }
+
+    return organization_id_dict
+
+
+def add_edges_person_organization(
+    g: GraphTraversalSource,
+    contact_df: pd.DataFrame,
+    person_id_dict: dict,
+    organization_id_dict: dict
+):
+    query_executor = BulkQueryExecutor(g, 100)
+
+    for _, row in tqdm(
+        contact_df.iterrows(),
+        total=contact_df.shape[0],
+        desc="Adding edges"
+    ):
+        person_uid_value = row["Email"] if pd.notnull(row["Email"]) else "_".join([row["Full Name"], row["Organization"]]).replace(" ", "_").lower()
+        organization_uid_value = row["Organization"].lower().capitalize()
+
+        person_graph_id = person_id_dict.get(person_uid_value)
+        organization_graph_id = organization_id_dict.get(organization_uid_value)
+
+        g.V(organization_graph_id).addE("associated with").to(__.V(person_graph_id)).iterate()
+
+    query_executor.force_execute()
+
+
+# %%
+contact_df = pd.read_csv("data/2019-2023_Leads_List_Test.csv")
