2f9bd228902d0ebc464281fc011d9060e873339d
Nicole T. <nbtrieu@uci.edu>
2024-07-01 12:53:52 -0700

    Update property keys

--- app/data_objects/person.py
+++ app/data_objects/person.py
@@ -3,12 +3,20 @@ class Person:
     LABEL = "person"
 
     class PropertyKey:
-        UID = "uid"  # same as email
-        NAME = "name"
+        UUID = "uuid"  # via random UUID generator
+        FIRST_NAME = "first_name"
+        LAST_NAME = "last_name"
+        FULL_NAME = "full_name"
         EMAIL = "email"
         PHONE = "phone"
+        SOCIAL_MEDIA = "social_media"
+        DOB = "dob"
         TITLE = "title"
+        ORGANIZATION = "organization"
         MAILING_ADDRESS = "mailing_address"
         INTEREST_AREAS = "interest_areas"
         LEAD_SOURCE = "lead_source"
         EVENT_NAME = "event_name"
+        INGESTION_TAG = "ingestion_tag"
+        PURCHASING_AGENT = "purchasing_agent"
+        VALIDATED_LEAD_STATUS = "validated_lead_status"
